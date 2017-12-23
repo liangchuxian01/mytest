429dde4c25acad4a601aea41fcd225a3f9d845ee
lcx <15767979837@163.com>
2017-12-23 19:51:07 +0800

    删

--- jd/src/main/java/cn/itheima/service/SearchService.java
+++
@@ -1,9 +0,0 @@
-package cn.itheima.service;
-
-import java.util.List;
-
-import cn.itheima.po.Product;
-
-public interface SearchService {
-	List<Product> searchProduct(String queryString,String catalog_name,String price,String page,String sort);
-}
